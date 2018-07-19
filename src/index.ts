b3e70d0dcd9635e364f708de0d5b1ae0b1a9cf88
Flynn <wphestiraid@gmail.com>
2018-07-19 14:28:54 +0900

    TypeORM 튜토리얼 진행 1
    - Entity 생성 및 데이터 생성 후 입력

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import { User } from './entity/User';
+import { Photo } from './entity/Photo'
 import connectionOptions from './connectionOptions';
 
 createConnection(connectionOptions)
   .then(async connection => {
-    console.log('Inserting a new user into the database...');
-    const user = new User();
-    user.firstName = 'Timber';
-    user.lastName = 'Saw';
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log('Saved a new user with id: ' + user.id);
+    let photo = new Photo();
+    photo.name = 'Me and Bears';
+    photo.description = 'I am near polar bears';
+    photo.filename = 'photo-with-bears.jpg';
+    photo.views = 1;
+    photo.isPublished = true;
 
-    console.log('Loading users from the database...');
-    const users = await connection.manager.find(User);
-    console.log('Loaded users: ', users);
-
-    console.log('Here you can setup and run express/koa/any other framework.');
+    return connection.manager.save(photo).then(photo => { console.log('Photo has been saved. Photo id is', photo.id )})
   })
   .catch(error => console.log(error));
