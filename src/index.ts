27c63d89a50f0d62bd4b55db25c2c36e85e9b58d
Flynn <wphestiraid@gmail.com>
2018-07-20 10:21:30 +0900

    TypeORM 데이터 저장 및 가져오기

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import { Photo } from './entity/Photo'
+import { Photo } from './entity/Photo';
 import connectionOptions from './connectionOptions';
 
 createConnection(connectionOptions)
@@ -12,6 +12,30 @@ createConnection(connectionOptions)
     photo.views = 1;
     photo.isPublished = true;
 
-    return connection.manager.save(photo).then(photo => { console.log('Photo has been saved. Photo id is', photo.id )})
+    let photoRepository = connection.getRepository(Photo);
+
+    await photoRepository.save(photo);
+    console.log('Photo has been saved');
+
+    let allPhotos = await photoRepository.find();
+    console.log('All photos from the db: ', allPhotos);
+
+    let firstPhoto = await photoRepository.findOne(1);
+    console.log('First photo from the db: ', firstPhoto);
+
+    let meAndBearsPhoto = await photoRepository.findOne({
+      name: 'Me and Bears'
+    });
+    console.log('Me and Bears photo from the db: ', meAndBearsPhoto);
+
+    let allViewedPhotos = await photoRepository.find({ views: 1 });
+    console.log('All viewed photos: ', allViewedPhotos);
+
+    let allPublishedPhotos = await photoRepository.find({ isPublished: true });
+    console.log('All published photos: ', allPublishedPhotos);
+
+    let [findAllPhotos, findPhotosCount] = await photoRepository.findAndCount();
+    console.log('All photos: ', findAllPhotos);
+    console.log('Photos count: ', findPhotosCount);
   })
   .catch(error => console.log(error));
